f5f02ce24d3dad953a8ea934cc1eacda621984a7
meghakundu <54468245+meghakundu@users.noreply.github.com>
2024-01-21 07:42:07 +0530

    Update index.py

--- simple_email/index.py
+++ simple_email/index.py
@@ -4,7 +4,7 @@ def lambda_handler(event, context):
     # TODO implement
 
     client = boto3.client('sns')
-    snsArn = 'arn:aws:sns:us-east-1:257440051472:testmsg'
+    #snsArn = 'arn:aws:sns:us-east-1:257440051472:testmsg'
     message = event['Records'][0]['Sns']['Message']
 
     response = client.publish(
